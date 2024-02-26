98183a110fbc107e0d7139e75172eb64d7e9d14e
cah-patrick-gabala <patrick.gabala@cardinalhealth.com>
2024-02-26 10:58:19 -0500

    main: fixes from veracode

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson8.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson8.java
@@ -36,6 +36,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.sql.PreparedStatement;
 
 @RestController
 @AssignmentHints(
